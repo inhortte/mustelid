03dbfe4ad36037fc18041dabb3583fc027bbf66d
Larger One <inhortte@gmail.com>
2010-11-01 16:42:00 -0500

    Subfamilies editing working now, plus a cancel button added to the new/edit form.

--- public/javascript/mustelids.js
+++ public/javascript/mustelids.js
@@ -3,6 +3,16 @@ link_assoc["subfamily"] = "subfam";
 link_assoc["genus"] = "gen";
 link_assoc["species"] = "druh";
 
+function get_form_link(form_id) {
+    var path = $("#" + form_id).attr("action").split("/");
+    // In case we are editing instead of creating a new model,
+    // the path will be /admin/prefix/id, and 'id' needs to be expunged.
+    if(path.length == 4) {
+	path.pop();
+    }
+    return path.pop();
+}
+
 $(document).ready(function() {
     $("#headmustelid img").fadeIn("slow");
     $("h5").prepend("(").append(")");
@@ -16,13 +26,7 @@ $(document).ready(function() {
     // Admin form submits.
     // First, for new entries and entries being edited.
     $("#admin_button").click(function() {
-	var path = $("#admin_form").attr("action").split("/");
-	// In case we are editing instead of creating a new model,
-	// the path will be /admin/prefix/id, and 'id' needs to be expunged.
-	if(path.length == 4) {
-	    path.pop();
-	}
-	var prefix = link_assoc[path.pop()];
+	var prefix = link_assoc[get_form_link("admin_form")];
 	if(prefix == "gen") {
 	    $("#" + prefix + "_subfam_id").val($("#subfam_select").val());
 	}
@@ -30,6 +34,9 @@ $(document).ready(function() {
 	    $("#" + prefix + "_gen_id").val($("#gen_select").val());
 	}
 	$("#admin_form").submit();
+    });
+    $("#admin_cancel_button").click(function() {
+	$(location).attr('href', "/admin/" + get_form_link("admin_form"));
     });
     // Secondly, for showing and hiding blocks of substrata.
     $("#latin_list > .line > .entry > a").click(function() {
